ee851bb5aeebab008348ce9f666a683f40d7be88
“Tommy <Tommy.nguyen@detigroup.vn>
2025-12-06 15:44:25 +0700

    stop bot

--- clean.py
+++ clean.py
@@ -1,6 +1,7 @@
 import os
 import random
 import time
+from datetime import datetime
 
 
 
@@ -24,9 +25,7 @@ def main():
         users = get_user_model().objects.filter(~Q(is_staff=True) & ~Q(username__iregex="^bot[0-9]+@bot.com$")).all()
 
         wallets = UserWallet.objects.filter(user__in=users)
-        accumulation_state = AccumulationState.objects.filter(wallet__in=wallets.all())
-        AccumulationTransaction.objects.filter(accumulation_state__in=accumulation_state.all()).delete()
-        accumulation_state.delete()
+        AccumulationTransaction.objects.filter(wallet_transaction__wallet__in=wallets.all()).delete()
 
         BotConfig.objects.filter(user__in=users).delete()
         # MarketTradeRequest.objects.filter(user__in=users).delete()
@@ -38,10 +37,20 @@ def main():
 
         Balance.objects.update(amount=0, amount_in_orders=0)
         wallets2 = UserWallet.objects.filter(~Q(user__isnull=True) & ~Q(user__username__iregex="^bot[0-9]+@bot.com$"))
-        accumulation_state = AccumulationState.objects.filter(wallet__in=wallets2.all())
-        AccumulationTransaction.objects.filter(accumulation_state__in=accumulation_state.all()).delete()
-        accumulation_state.delete()
+        AccumulationTransaction.objects.filter(wallet_transaction__wallet__in=wallets2.all()).delete()
         wallets2.delete()
+
+        # Disable all bot configurations with timestamp
+        disable_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        all_bots = BotConfig.objects.filter(enabled=True)
+        bot_count = all_bots.count()
+        
+        for bot in all_bots:
+            bot.enabled = False
+            bot.stopped = True
+            bot.save()
+        
+        print(f'Disabled {bot_count} bot(s) at {disable_timestamp}')
 
 
 if __name__ == '__main__':
